afb84efcd1fd2d0cf513f121b15fd03805add649
Rudi Strydom <iam@thatguy.co.za>
2018-01-04 15:46:52 +0200

    Commenting out create / patch / update

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -23,43 +23,43 @@ class HeroesController < ApplicationController
 
   # POST /heroes
   # POST /heroes.json
-  def create
-    @hero = Hero.new(hero_params)
-
-    respond_to do |format|
-      if @hero.save
-        format.html { redirect_to @hero, notice: 'Hero was successfully created.' }
-        format.json { render :show, status: :created, location: @hero }
-      else
-        format.html { render :new }
-        format.json { render json: @hero.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /heroes/1
-  # PATCH/PUT /heroes/1.json
-  def update
-    respond_to do |format|
-      if @hero.update(hero_params)
-        format.html { redirect_to @hero, notice: 'Hero was successfully updated.' }
-        format.json { render :show, status: :ok, location: @hero }
-      else
-        format.html { render :edit }
-        format.json { render json: @hero.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def create
+  #   @hero = Hero.new(hero_params)
+  #
+  #   respond_to do |format|
+  #     if @hero.save
+  #       format.html { redirect_to @hero, notice: 'Hero was successfully created.' }
+  #       format.json { render :show, status: :created, location: @hero }
+  #     else
+  #       format.html { render :new }
+  #       format.json { render json: @hero.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+  #
+  # # PATCH/PUT /heroes/1
+  # # PATCH/PUT /heroes/1.json
+  # def update
+  #   respond_to do |format|
+  #     if @hero.update(hero_params)
+  #       format.html { redirect_to @hero, notice: 'Hero was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @hero }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @hero.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /heroes/1
   # DELETE /heroes/1.json
-  def destroy
-    @hero.destroy
-    respond_to do |format|
-      format.html { redirect_to heroes_url, notice: 'Hero was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  # def destroy
+  #   @hero.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to heroes_url, notice: 'Hero was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
